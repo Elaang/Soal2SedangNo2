5f88772293fe937b9051617f490bf59b89becf88
Elaangg <elang_syadewa_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 19:09:10 +0700

    Add files via upload

---
+++ GabunganSibau.java
@@ -0,0 +1,38 @@
+package Sekolah.UKL;
+import java.util.Scanner;
+
+public class GabunganSibau {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kamu mau input jari jari atau diameter tabung?");
+        System.out.println("jari-jari / diameter");
+        String pilihan = input.nextLine();
+        if (pilihan.equalsIgnoreCase("diameter")) {
+          System.out.print("Masukkan diameter alas tabung (d): ");
+            double d = input.nextDouble();
+            double r = d / 2;
+          System.out.print("Masukkan tinggi tabung (t): ");
+            double t = input.nextDouble();
+            double volume = sibau.hitungVolumeDiameter(r, t);
+          System.out.printf("Volume tabung dengan diameter " + d + " dan tinggi " + t + " adalah " + volume);
+          input.close();
+          return;
+        } else if (!pilihan.equalsIgnoreCase("jari-jari")) {
+            System.out.print("Masukkan jari-jari alas tabung (r): ");
+                double r = input.nextDouble();
+            System.out.print("Masukkan tinggi tabung (t): ");
+                double t = input.nextDouble();
+                double volume = sibau.hitungVolumeJariJari(r, t);
+            System.out.printf("Volume tabung dengan jari-jari " + r + " dan tinggi " + t + " adalah " + volume);
+        } else {
+          System.out.println("Rage bait used to be believable.");
+          System.out.println("You shall PERISH!");
+          System.out.println("Goodbye.");
+          System.out.println("--------------------------------");
+          System.out.println("anu, kepencet ini");
+          System.exit(0);
+        }
+        input.close();
+      }
+    
+}
---
+++ sibau.java
@@ -0,0 +1,13 @@
+package Sekolah.UKL;
+
+public class sibau {
+
+    public static double hitungVolumeJariJari(double r, double t) {
+        double volume = 3.14 * r * r * t; 
+        return volume;
+      }
+        static double hitungVolumeDiameter(double d, double t) {
+        double volume = 3.14 * d * t; 
+        return volume;
+      }
+}
